4a8b541487a10a02ca9d35165133d1791bce8d7a
Joe Esposito <joe.esposito@snapretail.com>
2013-02-06 12:55:50 -0500

    Spaces in name.

--- GoogleAppsClient/Properties/AssemblyInfo.cs
+++ GoogleAppsClient/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GoogleAppsClient")]
+[assembly: AssemblyTitle("Google Apps Client")]
 [assembly: AssemblyDescription("Access Google Apps and get notifications on your desktop.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
